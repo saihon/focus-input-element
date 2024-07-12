c16571b5576ebbb9db8e581aa84c8d57eccad50d
saihon <devqrx+saihon-github@gmail.com>
2024-07-12 14:26:18 +0900

    fix

--- src/ts/focusableElement.ts
+++ src/ts/focusableElement.ts
@@ -7,8 +7,8 @@ export class FocusableElement {
     public activate(preventScroll: boolean = false) {
         // preventScroll is set to true, the browser will not scroll until the element into view after focus.
         this.element.focus({ preventScroll: preventScroll });
-        if (this.element.hasOwnProperty("select")) {
-            (this.element as HTMLInputElement).select();
+        if ("select" in this.element) {
+            (this.element as any).select();
         }
     }
 
